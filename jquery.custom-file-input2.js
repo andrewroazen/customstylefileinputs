665952d9ba2ba0f78817fffe66467055140e20f6
andrewroazen <39416103+andrewroazen@users.noreply.github.com>
2021-09-17 18:24:37 -0700

    Create jquery.custom-file-input2.js
    The application of this code required a check for the file upload not being over a fixed limit, which in turn required refactoring the codebase to delegate (especially since our web application allows the user to have multiple sections with individual file uploads).
    I'll get around to rewriting the vanillaJS version of this.

---
+++ jquery.custom-file-input2.js
@@ -0,0 +1,34 @@
+$(document).on("change", ".inputfile", function (e) { // Improved custom file upload widget
+  /**
+   * Based on code from https://tympanus.net/codrops/2015/09/15/styling-customizing-file-inputs-smart-way/
+   * 
+   * Improvements: 
+   * - Now a single event bound to document instead of manually instantiated per instance
+   * - Blocks upload of a file over maximum filesize in bytes
+   * 
+   */
+  const maxfilesize = (501 * 1024 * 1024); // 501 Mb defined in bytes
+  var $input = $(this), 
+  fileLength = this.files[0].size,
+  $inputparent = $input.next('label'), 
+  labelVal = $inputparent.html();
+  if (fileLength <= maxfilesize) {
+    var fileName = '';
+    if (this.files && this.files.length > 1)
+      fileName = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length);
+    else if (e.target.value)
+      fileName = e.target.value.split('\\').pop();
+
+    if (fileName)
+      $inputparent.find('span').html(fileName);
+    else
+      $inputparent.html(labelVal);
+  } else { // File is too large; throw error message then remove input's file
+    alert("File too large, please choose another.");
+    $(this).val('');
+  }
+});
+// Firefox bug fix
+$(document)
+  .on('focus', '.inputfile', function() {$(this).addClass('has-focus')})
+  .on('blur', '.inputfile', function() {$(this).removeClass('has-focus')});
